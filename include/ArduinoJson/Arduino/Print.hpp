565f7eebcc3df68e7e39b48572cf54d35114ba93
firepick1 (localhost) <karl@firepick.org>
2015-07-17 20:34:34 -0700

    3-digits

--- include/ArduinoJson/Arduino/Print.hpp
+++ include/ArduinoJson/Arduino/Print.hpp
@@ -19,7 +19,7 @@ class Print {
   virtual size_t write(uint8_t) = 0;
 
   size_t print(const char[]);
-  size_t print(double, int = 2);
+  size_t print(double, int = 3);
   size_t print(long);
   size_t println();
 };
