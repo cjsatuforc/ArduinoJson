2dd69f22516251988f771474a2a4bd36eb8a8018
Paul Jones <paul@pauljones.id.au>
2016-02-19 15:38:46 +1100

    Update for Cortex M3 + M4 chips
    Signed-off-by: Paul Jones <paul@pauljones.id.au>

--- ArduinoJson.h
+++ ArduinoJson.h
@@ -4,7 +4,7 @@
 // Arduino JSON library
 // https://github.com/bblanchon/ArduinoJson
 
-#ifdef ARDUINO
+#if defined(ARDUINO) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7EM__)
 
 // This file is here to help the Arduino IDE find the other files.
 
